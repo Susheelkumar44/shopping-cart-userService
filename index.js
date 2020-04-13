0364d032d6b0aa909ec54924ca85449ba9150c06
Susheelkumar S <susheelkumar.3615@gmail.com>
2020-04-13 10:43:17 +0530

    Added CORS policy

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const usersRouter = require("./users/router/router")
 const logger = require("./utilities/logger");
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 1235
 
 require("./config/mongoose")(app);
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/user", usersRouter);
 
